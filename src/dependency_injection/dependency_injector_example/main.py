bd91a3362af9eb6a42b1eeb8512389dde8df9783
Sergio León <panicoenlaxbox@gmail.com>
2022-09-18 12:04:19 +0200

    Dependency Injector without @inject

--- src/dependency_injection/dependency_injector_example/main.py
+++ src/dependency_injection/dependency_injector_example/main.py
@@ -1,18 +1,24 @@
 from pprint import pprint
 
-from dependency_injector.wiring import Provide, inject
+from dependency_injector.wiring import Provide, inject  # noqa: F401
 
 from dependency_injection.dependency_injector_example.assets import Reader
 from dependency_injection.dependency_injector_example.containers import Container
 
+# @inject
+# def main(reader: Reader = Provide["reader"], path: str = Provide["path"]) -> None:
+#     pprint(reader.read(path))
 
-@inject
-def main(reader: Reader = Provide["reader"], path: str = Provide["path"]) -> None:
+
+def main(reader: Reader, path: str) -> None:
     pprint(reader.read(path))
 
 
 if __name__ == "__main__":
     # https://github.com/ets-labs/python-dependency-injector/issues/339#issuecomment-747820448
+    # container = Container()
+    # container.wire(modules=[__name__])
+    # main()
+
     container = Container()
-    container.wire(modules=[__name__])
-    main()
+    main(container.reader(), container.path())
